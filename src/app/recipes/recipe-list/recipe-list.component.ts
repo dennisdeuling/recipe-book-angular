e44822108e0e15e1348eb1f94940016be076061a
dennisdeuling <dennis.deuling@gmail.com>
2023-01-23 07:30:54 +0100

    setup the app

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+	selector: 'app-recipe-list',
+	templateUrl: './recipe-list.component.html',
+	styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+	recipes: Recipe[] = [
+		new Recipe(
+			'A Test Recipe',
+			'This is simply a test',
+			'https://i.pinimg.com/222x/a3/4c/08/a34c08589834ffce5366388a89ebeb27.jpg'
+		),
+		new Recipe(
+			'A Test Recipe',
+			'This is simply a test',
+			'https://cdn.donmai.us/original/c7/a8/c7a8635753999ac88f674ef2043668e1.jpg'
+		)
+	];
+}
