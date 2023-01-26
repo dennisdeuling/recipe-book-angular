fc0fc51c5a1a1ae7a39e6f15c1ffea78a9e000fb
dennisdeuling <dennis.deuling@gmail.com>
2023-01-26 16:26:53 +0100

    added service to the shopping list components

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,1 +1,16 @@
-export class ShoppingListService {}
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
+export class ShoppingListService {
+	ingredientsChanged = new EventEmitter<Ingredient[]>();
+	private ingredients: Ingredient[] = [new Ingredient('Apples', 5), new Ingredient('Tomatos', 10)];
+
+	addIngredient(ingredient: Ingredient) {
+		this.ingredients.push(ingredient);
+		this.ingredientsChanged.emit(this.ingredients.slice());
+	}
+
+	getIngredients() {
+		return this.ingredients.slice();
+	}
+}
